3f31baabab1ef9d027868bfe3149a15b6aee24e0
berk2s <berktopcu@outlook.com>
2021-11-04 14:21:39 +0300

    initial commit and dependencies configured

---
+++ src/main/java/com/berk2s/task/basketballregistration/BasketballRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.berk2s.task.basketballregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketballRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/berk2s/task/basketballregistration/BasketballRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.berk2s.task.basketballregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketballRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
